252ce85c0643cf76a806724c49873153406263c5
PetrovNikitaEvg <Shug1zawa@yandex.ru>
2023-10-15 22:52:37 +0500

    Task5

---
+++ Tyuiu.PetrovNE.Sprint2.Task5.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PetrovNE.Sprint2.Task5.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PetrovNE.Sprint2.Task5.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c63983a3-173e-4e7f-a796-e53369af336f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PetrovNE.Sprint2.Task5.V11/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PetrovNE.Sprint2.Task5.V11.Lib;
+
+namespace Tyuiu.PetrovNE.Sprint2.Task5.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Петров Н. Е.  | АСОиУБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Петров Никита Евгеньевич | АСОиУБ-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch, вычисляет       *");
+            Console.WriteLine("* требуемое значение и возвращает результат. Условие: Дата некоторого дня *");
+            Console.WriteLine("* характеризуется тремя натуральными числами: g (год), m (порядковый      *");
+            Console.WriteLine("* номер месяца) и n (число). По заданным g, n и m определить дату         *");
+            Console.WriteLine("* предыдущего дня. Заданный год не является високосным.                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+
+            Console.Write("Год = ");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Месяц = ");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("День = ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.FindDateOfNextDay(year, month, day);
+
+            Console.WriteLine("Дата следующего дня: " + res);
+
+            Console.ReadLine();
+        }
+    }
+}
+
